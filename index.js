73d985a327d06c1d19dbb9ceffdab8f293ffcb44
Aden Addis <adenwonde56@gmail.com>
2022-07-02 23:40:06 -0400

    commit tree 31d782800a67d842005331a55936bd5db712f9f3

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 function addingEventListener() {
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert("You clicked the button");
+// });
+
+
+
+// const button = document.getElementById("button")
+// console.log(button)
+// button.addEventListener("click", function() {
+//    alert("You clicked the button");
+// })
+
+
+// const clickButton = () => {
+//     alert("You clicked the button, yes you did")
+// }
+
+
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+   alert('I was clicked!')
+});
 }
+
+
+// const input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+
+
+
+
+
+
